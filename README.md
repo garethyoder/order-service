205c06cebb68bdc1e8d14bb6dfd00a9579ffca3d
Gareth Yoder <garethyoder@yahoo.com>
2024-01-27 20:12:04 -0500

    Rename table and update sam deployment to use config-env

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ To create a bootable jar to be deployed to Lambda run:
 
 To deploy via SAM
 `sam deploy --stack-name order-service-test --capabilities CAPABILITY_IAM --resolve-s3`
+
+To deploy SAM for test:
+`sam deploy --config-env test`
+
+To deploy SAM for test:
+`sam deploy --config-env prod`
--- src/main/java/com/cedarmeadowmeats/orderservice/controller/OrderController.java
+++ src/main/java/com/cedarmeadowmeats/orderservice/controller/OrderController.java
@@ -4,6 +4,8 @@ import com.cedarmeadowmeats.orderservice.model.OrderFormSubmissionRequest;
 import com.cedarmeadowmeats.orderservice.model.Submission;
 import com.cedarmeadowmeats.orderservice.model.SubmissionRequest;
 import com.cedarmeadowmeats.orderservice.service.OrderService;
+import com.cedarmeadowmeats.orderservice.service.SquareService;
+import com.squareup.square.models.Customer;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,14 +13,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.List;
+
 @RestController
 @EnableWebMvc
 public class OrderController {
 
     private final OrderService orderService;
+    private final SquareService squareService;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, SquareService squareService) {
         this.orderService = orderService;
+        this.squareService = squareService;
     }
 
     @PostMapping("/submit")
@@ -39,5 +45,10 @@ public class OrderController {
     @GetMapping("/ping2")
     public String ping2() {
         return "ok!";
+    }
+
+    @GetMapping("/customers")
+    public List<Customer> customers() {
+        return squareService.getCustomersByEmail("garethyoder@yahoo.com");
     }
 }
